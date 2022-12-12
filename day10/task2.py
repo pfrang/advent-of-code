08aa39a32ef99acc0b28ca24e022cf735ce89f4a
Peder Frang <peder.frang@gmail.com>
2022-12-12 15:07:59 +0100

    task2-day10

--- day10/task2.py
+++ day10/task2.py
@@ -1,32 +1,33 @@
 import re
 import json
-import numpy
+import sys
+
 f = open('input.txt', 'r')
 content = f.readlines()
 
 score = 1
 cycle = 0
-sprite_position = 1
+sprite_position = 0
 my_dict = {}
 
 def checkforCycle():
     global sprite_position
-    if(cycle == 40):
+    if(cycle == 41):
         sprite_position = 1
         print("\n")
-    elif(cycle == 80):
+    elif(cycle == 81):
         sprite_position = 1
         print("\n")
-    elif(cycle == 120):
+    elif(cycle == 121):
         sprite_position = 1
         print("\n")
-    elif(cycle == 160):
+    elif(cycle == 161):
         sprite_position = 1
         print("\n")
-    elif(cycle == 200):
+    elif(cycle == 201):
         sprite_position = 1
         print("\n")
-    elif(cycle == 240):
+    elif(cycle == 241):
         sprite_position = 1
         print("\n")
     if(sprite_position in [score, score +1, score+2]):
@@ -34,7 +35,8 @@ def checkforCycle():
     else:
         print(".",end="")
 
-for row in content:
+
+for index,row in enumerate(content):
     row = row.replace('\n','')
     if("addx" in row):
         nr = int(row.split(" ")[1])
